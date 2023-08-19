67366dd1c3d94e6022a7067c30a43a4f48005524
碳烤黄蜂 <tankaohuangfeng@foxmail.com>
2023-08-20 00:04:57 +0800

    修改管理器的功能开发指南

--- QiancizhanKernel/src/test/java/ParserAndManagerTest.java
+++ QiancizhanKernel/src/test/java/ParserAndManagerTest.java
@@ -1,5 +1,6 @@
 import com.potato.Config;
 import com.potato.Log.Log;
+import com.potato.Manager.AutoManager;
 import com.potato.Parser.AutoParser;
 import com.potato.ToolKit.WordFileType;
 import com.potato.Word.Word;
@@ -32,5 +33,34 @@ public class ParserAndManagerTest
         // 在Config中修改后则会使用我们提供的修改器
         AutoParser parser = new AutoParser(file);
         List<Word> wordList = parser.getWordList();
+    }
+
+    @Test
+    public void managerTest()
+    {
+        // 首先要在Config中配置好Manager
+        /*
+        try
+        {
+            Config.setManager(WordFileType.DATABASE, MyManager.class.getConstructor(File.class));
+        }
+        catch (NoSuchMethodException e)
+        {
+            Log.e(getClass().toString(),
+                String.format("将文件类型%s的管理器替换为%s时出现错误", WordFileType.DATABASE.type(), MyManager.class), e);
+            throw new RuntimeException(e);
+        }
+         */
+        File file = new File("a.db");
+        // AutoManager会根据文件类型自动获取对应的管理器
+        // 数据库文件的默认管理器是内核中提供的DatabaseManager
+        // 在Config中修改后则会使用我们提供的管理器
+        AutoManager manager = new AutoManager(file);
+
+        // Word构造方法请见API指南
+        Word insertWord = new Word.WordBuilder().name("test").build();
+        manager.insert(insertWord);  // 插入单词
+
+        manager.push();  // 将修改写入单词本文件
     }
 }
